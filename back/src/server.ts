1997763483cec18d20d07dc8415f3e9bb06eb762
Kayke Galdiano <kaykegaldiano@gmail.com>
2025-05-29 00:22:19 -0300

    fix: update server log message to English and format package.json scripts

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
+  console.log(`API running on port ${PORT}`);
 });
