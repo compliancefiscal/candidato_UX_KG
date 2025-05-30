dfd0d24ed6355e68c4f019bb67a626485eef2e71
Kayke Galdiano <kaykegaldiano@gmail.com>
2025-05-30 00:19:32 -0300

    feat: add CORS support to the application and update package dependencies

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import employeeRouter from './routes/employee.routes';
 import { errorHandler } from './middlewares/error.handler';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/employees', employeeRouter);
 app.use(errorHandler);
