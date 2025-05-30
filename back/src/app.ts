467bf1135a0f3ff2eb846a9fae4a42a700f879b9
Kayke Galdiano <kaykegaldiano@gmail.com>
2025-05-30 13:33:39 -0300

    feat: add user router to handle user-related API endpoints

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import employeeRouter from './routes/employee.routes';
+import userRouter from './routes/user.routes';
 import { errorHandler } from './middlewares/error.handler';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRouter);
 app.use('/api/employees', employeeRouter);
+
 app.use(errorHandler);
 
 export default app;
